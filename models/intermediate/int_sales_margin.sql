01211b624834f1914411e7356d174052625cafc8
jean carlos campos valverde <jcamposvalverde@gmail.com>
2024-08-27 20:32:58 +0000

    sales_margin_v2

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,9 +1,11 @@
 SELECT 
     products_id,
-    purchase_price,
+    CAST(purchase_price as float64) AS purchase_price,
     quantity,
     revenue,
-    ---purchase_price*quantity AS purchase_cost,
-    ---revenue-(purchase_price*quantity) AS margin
+    CAST(purchase_price as float64)*quantity AS purchase_cost,
+    ROUND(revenue-(CAST(purchase_price as float64)*quantity),2) AS margin
+FROM {{ref("stg_raw__sales")}}
 JOIN {{ref("stg_raw__product")}}
 USING(products_id)
+WHERE CAST(purchase_price AS FLOAT64) IS NOT NULL
