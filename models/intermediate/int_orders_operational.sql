002672afbd651db9b1cbf5f6356923cb3244dc02
jean carlos campos valverde <jcamposvalverde@gmail.com>
2024-08-27 21:05:04 +0000

    orders_operational

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,8 @@
+SELECT 
+    orders_id,
+    date_date,
+    ROUND(margin+shipping_fee-logcost-CAST(ship_cost as float64),2) AS operational_margin
+FROM {{ref("int_orders_margin")}}
+JOIN {{ref("stg_raw__ship")}}
+USING(orders_id)
+ORDER BY orders_id DESC
