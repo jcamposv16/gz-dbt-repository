670f546cb4ec30556224611072302ff1a371842a
Jean Carlos Campos Valverde <jcamposvalverde@gmail.com>
2024-08-28 20:33:30 +0000

    Campaign_day

---
+++ models/intermediate/int_campaigns_day.sql
@@ -0,0 +1,7 @@
+SELECT date_date,
+ROUND(SUM(ads_cost),2) AS ads_cost,
+ROUND(SUM(impression),2) AS ads_impression,
+ROUND(SUM(click),2) AS ads_clicks,
+FROM {{ref("int_campaigns")}}
+GROUP BY date_date
+ORDER BY date_date ASC
